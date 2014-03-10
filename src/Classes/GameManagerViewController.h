64c4dafa6c35737086baa0a3e9078fd77f58a7f6
ivanfer <sanchezivanf@gmail.com>
2014-03-10 00:18:09 -0400

    Removed duplicate directory

---
+++ src/Classes/GameManagerViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameManagerViewController.h
+//  Naval Knockout
+//
+//  Created by ivanfer on 2014-03-09.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameManagerViewController : UIViewController
+
+@end
---
+++ src/Classes/GameManagerViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameManagerViewController.m
+//  Naval Knockout
+//
+//  Created by ivanfer on 2014-03-09.
+//
+//
+
+#import "GameManagerViewController.h"
+
+@interface GameManagerViewController ()
+
+@end
+
+@implementation GameManagerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
