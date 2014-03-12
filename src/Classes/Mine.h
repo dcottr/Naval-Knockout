41f4a49e5eb21260cffbac0f18f3cd0bf4b9d82e
dcottr <davidrcottrell@gmail.com>
2014-03-12 15:25:03 -0400

    Drop Mine functionality

---
+++ src/Classes/Mine.h
@@ -0,0 +1,13 @@
+//
+//  Mine.h
+//  Naval Knockout
+//
+//  Created by David Cottrell on 2014-03-12.
+//
+//
+
+#import "SPSprite.h"
+
+@interface Mine : SPSprite
+
+@end
---
+++ src/Classes/Mine.m
@@ -0,0 +1,13 @@
+//
+//  Mine.m
+//  Naval Knockout
+//
+//  Created by David Cottrell on 2014-03-12.
+//
+//
+
+#import "Mine.h"
+
+@implementation Mine
+
+@end
